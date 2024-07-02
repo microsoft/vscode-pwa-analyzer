40b5d77c2b7d16ccecf79f91a8b788ca974e1cb4
Connor Peet <connor@peet.io>
2024-07-02 10:57:36 -0700

    fix: decompress partially-collected logs
    Closes https://github.com/microsoft/vscode-js-debug/issues/1931

--- src/file-upload.tsx
+++ src/file-upload.tsx
@@ -2,8 +2,8 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
+import { Gunzip, gunzipSync, strFromU8 } from 'fflate';
 import * as React from 'react';
-import { inflate } from 'pako';
 
 export const FileUpload: React.FC<{ onChange(text: string): void }> = props => {
   const uploadFile = React.useCallback((evt: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,11 +15,30 @@ export const FileUpload: React.FC<{ onChange(text: string): void }> = props => {
       }
 
       const result = new Uint8Array(reader.result as ArrayBuffer);
+
+      // first try decompressing as a complete stream:
+      let err: Error;
       try {
-        props.onChange(inflate(result, { to: 'string' }));
+        props.onChange(strFromU8(gunzipSync(result)));
+        return;
       } catch (e) {
-        alert(`Error decompressing file: ${e}`);
+        err = e as Error;
       }
+
+      // may error if the log was collected before the session ends, so try as
+      // a stream and decompress as much as we can:
+      const finish = debounce(() => props.onChange(text), 100);
+      const showAlert = setTimeout(() => alert(`Error decompressing file: ${err}`), 3000);
+
+      let text: string;
+      const decoder = new TextDecoder();
+      const ds = new Gunzip(chunk => {
+        text = decoder.decode(chunk, { stream: true });
+        clearTimeout(showAlert);
+        finish();
+      });
+
+      ds.push(result);
     };
 
     const file = evt.target.files?.[0];
@@ -35,4 +54,15 @@ export const FileUpload: React.FC<{ onChange(text: string): void }> = props => {
   }, []);
 
   return <input type="file" onChange={uploadFile} />;
+};
+
+const debounce = (func: () => void, delay: number) => {
+  let timeoutId: number;
+
+  return () => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func();
+    }, delay);
+  };
 };
